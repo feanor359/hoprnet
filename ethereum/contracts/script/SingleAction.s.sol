ba0eaa2e9f508ad7ade638f4d72a598464573716
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2024-01-27 08:05:53 +0800

    skip reading private key for hd wallet

--- ethereum/contracts/script/SingleAction.s.sol
+++ ethereum/contracts/script/SingleAction.s.sol
@@ -103,12 +103,7 @@ contract SingleActionFromPrivateKeyScript is Test, NetworkConfig {
         // 2. Get private key of caller
         // Set to default when it's in development environment (uint for
         // 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80)
-        uint256 privateKey = currentEnvironmentType == EnvironmentType.LOCAL
-            ? 77_814_517_325_470_205_911_140_941_194_401_928_579_557_062_014_761_831_930_645_393_041_380_819_009_408
-            : vm.envUint("PRIVATE_KEY");
-        msgSender = vm.addr(privateKey);
-        emit log_named_address("msgSender address", msgSender);
-        vm.startBroadcast(privateKey);
+        vm.startBroadcast();
     }
 
     /**
