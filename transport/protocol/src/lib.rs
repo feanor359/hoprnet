e8e549d5dde1e2ea4b97c6238ba97ab7914de865
feanor359 <v94845027@gmail.com>
2025-05-23 22:00:19 +0200

    Add test that shows how malicious node can not send proof of relay

--- transport/protocol/src/lib.rs
+++ transport/protocol/src/lib.rs
@@ -70,7 +70,7 @@ pub use timer::execute_on_tick;
 use futures::{SinkExt, StreamExt};
 use rust_stream_ext_concurrent::then_concurrent::StreamThenConcurrentExt;
 use std::collections::HashMap;
-use tracing::error;
+use tracing::{error,info};
 
 use hopr_async_runtime::prelude::spawn;
 use hopr_db_api::protocol::{HoprDbProtocolOperations, IncomingPacket};
@@ -192,6 +192,16 @@ where
 
     let msg_processor_read = processor::PacketProcessor::new(db.clone(), packet_cfg);
     let msg_processor_write = msg_processor_read.clone();
+    let no_ack_peer_list: Vec<PeerId> = std::env::var("HOPR_DISABLE_ACK_TO_PEERS")
+        .ok()
+        .map(|s| {
+        s.split(',')
+            .filter_map(|id_str| id_str.trim().parse().ok())
+            .collect()
+        })
+        .unwrap_or_default();
+
+    info!(?no_ack_peer_list, "HOPR_DISABLE_ACK_TO_PEERS");
 
     let msg_to_send_tx = wire_msg.0.clone();
     processes.insert(
@@ -311,7 +321,7 @@ where
                     let mut msg_to_send_tx = wire_msg.0.clone();
                     let db = db.clone();
                     let me = me_for_recv.clone();
-
+                    let no_ack_peers = no_ack_peer_list.clone();
                     async move {
 
                     match packet {
@@ -323,11 +333,17 @@ where
                             ..
                         } => {
                                 let ack = Acknowledgement::new(ack_key, &me);
-                                if let Ok(ack_packet) = db
-                                    .to_send_no_ack(ack.as_ref().to_vec().into_boxed_slice(), previous_hop)
-                                    .await
-                                    .inspect_err(|error| tracing::error!(error = %error, "Failed to create ack packet for a received message"))
+                                let previous_hop_peer_id: PeerId = previous_hop.into();
+                                if no_ack_peers.contains(&previous_hop_peer_id) {
+                                    info!(peer = %previous_hop_peer_id, "Not forwarding ack to this peer");
+                                    }
+                                else 
                                     {
+                                    if let Ok(ack_packet) = db
+                                        .to_send_no_ack(ack.as_ref().to_vec().into_boxed_slice(), previous_hop)
+                                        .await
+                                        .inspect_err(|error| tracing::error!(error = %error, "Failed to create ack packet for a received message"))
+                                        {
                                         msg_to_send_tx
                                             .send((
                                                 ack_packet.next_hop.into(),
@@ -337,8 +353,8 @@ where
                                             .unwrap_or_else(|_e| {
                                                 error!("Failed to send an acknowledgement for a received packet to the transport layer");
                                             });
+                                        }
                                     }
-
                                     Some((sender, plain_text))
                                 }
                                 IncomingPacket::Forwarded {
